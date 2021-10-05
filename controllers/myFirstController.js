d7ceac956817ab1ccf7166a4ef097869666d7383
unknown <mgaspar@nsign.tv>
2021-10-05 19:20:26 +0200

    Changed controller to response students name

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -1,6 +1,8 @@
 //req = request, res = response
 module.exports.helloWorld = (req, res) => {
-    const responseObj = {msg: `Hello World!`};
+
+    alumnos = ["Gerard","Khadija","Alex","Roger","Xavier","Eric","Miquel","Alba","Raul","Marc","Jordi","Marc","Pol","Biel", "Keen", "Alvar", "Pau", "Dani", "Jordi", "Jordi", "Alberto"]
+    const responseObj = {alumnes: alumnos , total: alumnos.length};
     const responseStatus = 200;
     res.status(responseStatus).send(responseObj);
 }
