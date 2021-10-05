68a66dc58872fcc50557f47bf7bd73d0ea62ab54
unknown <mgaspar@nsign.tv>
2021-10-05 19:22:27 +0200

    Changed reponse

--- controllers/myFirstController.js
+++ controllers/myFirstController.js
@@ -2,7 +2,7 @@
 module.exports.helloWorld = (req, res) => {
 
     alumnos = ["Gerard","Khadija","Alex","Roger","Xavier","Eric","Miquel","Alba","Raul","Marc","Jordi","Marc","Pol","Biel", "Keen", "Alvar", "Pau", "Dani", "Jordi", "Jordi", "Alberto"]
-    const responseObj = {alumnes: alumnos , total: alumnos.length};
+    const responseObj = {"alumnes": alumnos , "total": alumnos.length};
     const responseStatus = 200;
     res.status(responseStatus).send(responseObj);
 }
