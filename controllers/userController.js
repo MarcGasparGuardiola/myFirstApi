acb1bd25c7c80526294ad0f8f8906b3a3159d155
unknown <mgaspar@nsign.tv>
2021-10-19 19:19:05 +0200

    Changes after Routes lecture

---
+++ controllers/userController.js
@@ -0,0 +1,29 @@
+const users = [{id:1, name:'john'}, {id:2, name:'david'}, {id:3, name:'maria'}];
+
+const status = {
+    ok:200,
+    notFound: 404,
+}
+
+module.exports = {
+
+    list: (req, res) => {
+        //console.log(req.query)
+        console.log(req.body);
+        //Peticio a la BBDD
+        res.status(status.ok).send(users);
+    },
+
+    profile:(req, res) => {
+        //Peticio a la BBDD per obtenir dades user
+        //console.log(req.params);
+        const user = users.find((user) => user.id == req.params.userid);
+
+        if (user) {
+            res.status(status.ok).send(user);
+        } else {
+            const msg = {error: 'User ID not found'};
+            res.status(status.notFound).send(msg);
+        }  
+    },
+};
--- index.js
+++ index.js
@@ -5,6 +5,8 @@ require('dotenv');
 
 const myFirstController = require('./controllers/myFirstController');
 
+app.use('/api/v1/user', require('./routes/userRoutes'))
+
 app.get('/', myFirstController.helloWorld);
 
 app.listen(process.env.PORT || 3000, () => {
